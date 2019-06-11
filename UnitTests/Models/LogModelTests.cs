cded7c8a4ba6e5124398efdb70d7c0a4624b8eed
Dylan Medina <medinad@uw.edu>
2019-06-11 07:52:41 -0700

    Fixed bug in LogModelTests.cs for Update with null

--- UnitTests/Models/LogModelTests.cs
+++ UnitTests/Models/LogModelTests.cs
@@ -39,17 +39,17 @@ namespace UnitTests.Models
         }
 
         [TestMethod]
-        public void LogModel_Update_Invalid_Null_Data_Should_Fail()
+        public void LogModel_Update_Invalid_Null_Data_Should_Pass()
         {
 
             // Arange
-            var myTest = new LogModel(); 
+            var myTest = new LogModel();
 
             // Act
             var result = myTest.Update(null);
 
             // Assert
-            Assert.AreEqual("abc", myTest.PhoneID);
+            Assert.AreEqual(null, myTest.PhoneID);
         }
 
         [TestMethod]
